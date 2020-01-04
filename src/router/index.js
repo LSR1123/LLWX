a2321a6468c8208322b02bb5d7f4e7e579a8ac90
LSR1123 <939268457@qq.com>
2020-01-04 09:27:40 +0800

    下载此版本

--- src/router/index.js
+++ src/router/index.js
@@ -55,109 +55,75 @@ export const constantRoutes = [
     }]
   },
 
-  {
-    path: '/example',
-    component: Layout,
-    redirect: '/example/table',
-    name: 'Example',
-    meta: { title: 'Example', icon: 'example' },
-    children: [
-      {
-        path: 'table',
-        name: 'Table',
-        component: () => import('@/views/table/index'),
-        meta: { title: 'Table', icon: 'table' }
-      },
-      {
-        path: 'tree',
-        name: 'Tree',
-        component: () => import('@/views/tree/index'),
-        meta: { title: 'Tree', icon: 'tree' }
-      }
-    ]
-  },
+//  {
+//     path: '/example',
+//     component: Layout,
+//     redirect: '/example/table',
+//     name: 'Example',
+//     meta: { title: 'Example', icon: 'example' },
+//     children: [
+//       {
+//         path: 'table',
+//         name: 'Table',
+//         component: () => import('@/views/table/index'),
+//         meta: { title: 'Table', icon: 'table' }
+//       },
+//       {
+//         path: 'tree',
+//         name: 'Tree',
+//         component: () => import('@/views/tree/index'),
+//         meta: { title: 'Tree', icon: 'tree' }
+//       }
+//     ]
+//   },
 
   {
-    path: '/form',
+    path: '/customer',
     component: Layout,
     children: [
       {
-        path: 'index',
+        path: 'list',
         name: 'Form',
-        component: () => import('@/views/form/index'),
-        meta: { title: 'Form', icon: 'form' }
+        component: () => import('@/page/customer/list.vue'),
+        meta: { title: '顾客管理', icon: 'user' }
       }
     ]
   },
-
   {
-    path: '/nested',
+    path: '/employee',
     component: Layout,
-    redirect: '/nested/menu1',
-    name: 'Nested',
-    meta: {
-      title: 'Nested',
-      icon: 'nested'
-    },
     children: [
       {
-        path: 'menu1',
-        component: () => import('@/views/nested/menu1/index'), // Parent router-view
-        name: 'Menu1',
-        meta: { title: 'Menu1' },
-        children: [
-          {
-            path: 'menu1-1',
-            component: () => import('@/views/nested/menu1/menu1-1'),
-            name: 'Menu1-1',
-            meta: { title: 'Menu1-1' }
-          },
-          {
-            path: 'menu1-2',
-            component: () => import('@/views/nested/menu1/menu1-2'),
-            name: 'Menu1-2',
-            meta: { title: 'Menu1-2' },
-            children: [
-              {
-                path: 'menu1-2-1',
-                component: () => import('@/views/nested/menu1/menu1-2/menu1-2-1'),
-                name: 'Menu1-2-1',
-                meta: { title: 'Menu1-2-1' }
-              },
-              {
-                path: 'menu1-2-2',
-                component: () => import('@/views/nested/menu1/menu1-2/menu1-2-2'),
-                name: 'Menu1-2-2',
-                meta: { title: 'Menu1-2-2' }
-              }
-            ]
-          },
-          {
-            path: 'menu1-3',
-            component: () => import('@/views/nested/menu1/menu1-3'),
-            name: 'Menu1-3',
-            meta: { title: 'Menu1-3' }
-          }
-        ]
+        path: 'list',
+        name: 'Form',
+        component: () => import('@/page/employee/list.vue'),
+        meta: { title: '员工管理', icon: 'user' }
+      }
+    ]
+  },
+  
+  {
+    path: '/page',
+    //component: () => import('@/page/nested/index.vue'), // Parent router-view
+    component: Layout,
+    name: 'Form',
+    meta: { title: '系统配置', icon: 'user' },
+    children: [
+      {
+        path: '/page/category',
+        component: () => import('@/page/category/list.vue'),
+        name: '栏目管理',
+        meta: { title: '栏目管理' }
       },
       {
-        path: 'menu2',
-        component: () => import('@/views/nested/menu2/index'),
-        meta: { title: 'menu2' }
-      }
+        path: '/page/product',
+        component: () => import('@/page/product/list.vue'),
+        name: '产品管理',
+        meta: { title: '产品管理' }
+      },
     ]
-  },
-
-  {
-    path: 'external-link',
-    component: Layout,
-    children: [
-      {
-        path: 'https://panjiachen.github.io/vue-element-admin-site/#/',
-        meta: { title: 'External Link', icon: 'link' }
-      }
-    ]
-  },
+},
+ 
 
   // 404 page must be placed at the end !!!
   { path: '*', redirect: '/404', hidden: true }
